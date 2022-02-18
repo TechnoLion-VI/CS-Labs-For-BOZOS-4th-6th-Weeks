40a046c78309b882805d4ee0b901482749b336f1
Masaab Farhan <36335899+TechnoLion-VI@users.noreply.github.com>
2022-02-17 20:08:23 -0600

    Add files via upload

---
+++ AVL Voting Block/AVLRunner.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class AVLRunner {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		AVLTreeGraphics a=new AVLTreeGraphics("HI");
+	}
+
+}
